f2694003a5a0c62c83ce523fe5ea7430d568fbac
roney ruiz <roneyruizrojas@gmail.com>
2025-11-17 23:30:10 -0600

    chore: rename variables

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,6 @@ import (
 )
 
 func main() {
-	randomNum := helpers.RandomInt(1, 100)
-	fmt.Printf("Random number: %d\n", randomNum)
+	random := helpers.RandomInt(1, 100)
+	fmt.Printf("Random number: %d\n", random)
 }
