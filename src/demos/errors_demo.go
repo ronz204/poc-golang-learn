287f3141bf4a96ed660226ae5543b40abcd0f501
roney ruiz <roneyruizrojas@gmail.com>
2025-12-20 21:07:27 -0600

    feat: learn about error types

--- src/demos/errors_demo.go
+++ src/demos/errors_demo.go
@@ -1,18 +1,32 @@
 package demos
 
-import "errors"
-
-var (
-	ErrInvalidName = errors.New("invalid name provided")
-	ErrInvalidAge  = errors.New("invalid age provided")
+import (
+	"errors"
+	"fmt"
 )
+
+type InvalidNameError struct {
+	Name string
+}
+
+func (e *InvalidNameError) Error() string {
+	return fmt.Sprintf("invalid name: %s", e.Name)
+}
+
+type InvalidAgeError struct {
+	Age int
+}
+
+func (e *InvalidAgeError) Error() string {
+	return fmt.Sprintf("invalid age: %d", e.Age)
+}
 
 func RegisterUser(name string, age int) error {
 	if name == "" {
-		return ErrInvalidName
+		return &InvalidNameError{Name: name}
 	}
 	if age < 0 {
-		return ErrInvalidAge
+		return &InvalidAgeError{Age: age}
 	}
 	return nil
 }
@@ -20,11 +34,18 @@ func RegisterUser(name string, age int) error {
 func ErrorsDemo() {
 	err := RegisterUser("", 25)
 
-	if errors.Is(err, ErrInvalidName) {
-		println("Error: The name provided is invalid.")
+	if err == nil {
+		return
 	}
 
-	if errors.Is(err, ErrInvalidAge) {
-		println("Error: The age provided is invalid.")
+	var ageErr *InvalidAgeError
+	var nameErr *InvalidNameError
+
+	if errors.As(err, &ageErr) {
+		fmt.Println("Age error occurred:", ageErr)
+	}
+
+	if errors.As(err, &nameErr) {
+		fmt.Println("Name error occurred:", nameErr)
 	}
 }
